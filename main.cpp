da10575cef784d872b4395c5a5310c533d16018e
Saurav <sauravsaurav0@icloud.com>
2021-12-21 16:27:01 +0530

    Class Based Linked List

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 
-int main(){
+using namespace std;
 
 
+class Node {
+
+private:
+    int Data_Part;
+    Node* pointer_to_next {nullptr};
+    
+public:
+    void set_Data_Part(){
+        
+    }
+    
+    int  get_Data_Part(){
+        
+    }
+    
+    Node *get_Ptr(){
+        
+    }
+    
+
+};
+
+
+
+
+
+int main() {
+
+    Node *HeadPtr{nullptr}, *TailPtr{nullptr}, *temp{nullptr};
+
+ 
 
     return 0;
 }
