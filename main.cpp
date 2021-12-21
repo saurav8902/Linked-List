4813f662b24cf50ef1a0ce57b5e62c11de3790d4
Saurav <sauravsaurav0@icloud.com>
2021-12-21 16:08:14 +0530

    Defining Node for Linked List
     -> Defining the Node (Blueprint)
     -> Defining a Function to create Node
        * Parameter = Integer to Store the Data
        * Allocation Type = Dynamic
    ******* Yet to Do ********
     -> Proper Implementation of Singly Linked List

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 
+using namespace std;
+
+//Defining the Node
+struct Node {
+
+
+    int Data_Part;
+    Node* pointer_to_next;
+
+};
+
+//Function to Dynamically Create a Node
+
+Node* Create_Node(int Data){
+
+    Node *ptr = new Node;
+    ptr->Data_Part = Data ;
+    ptr->pointer_to_next = nullptr;
+
+    return ptr;
+}
+
 int main(){
 
 
+    Node *HeadPtr{nullptr}, *TailPtr{nullptr}, *temp{nullptr};
+    
+    HeadPtr = TailPtr = temp = Create_Node(10);
+    
+    cout<<"HeadPtr:"<< HeadPtr<<endl;
+    cout<<"temp : "<< temp <<endl;
+    cout<<"Tailptr:"<< TailPtr;
+
+    delete temp;
 
     return 0;
 }
